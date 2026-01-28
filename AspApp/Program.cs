1e24989fc5800b0bb8095778611a19a70138abce
hossein <hossein@mycompany.com>
2026-01-28 11:44:35 +0330

    [Asp,Ang] initial setup

---
+++ AspApp/Program.cs
@@ -0,0 +1,14 @@
+namespace AspApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
